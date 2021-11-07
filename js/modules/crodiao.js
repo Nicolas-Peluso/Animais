360933115f0f3954b89d004a207cbc3dda01f800
Nicolas peluso <pelusonicolas7@gmail.com>
2021-11-07 07:20:58 -0300

    pull

--- js/modules/crodiao.js
+++ js/modules/crodiao.js
@@ -1,9 +1,18 @@
-export default function Cordiao() {
-  const faqList = document.querySelectorAll('.faq-lista dt');
-  faqList.forEach((faq) => {
-    const dd = faq.nextElementSibling;
-    faq.addEventListener('click', () => {
-      dd.classList.toggle('showDD');
+export default class Cordiao {
+  constructor() {
+    this.faqList = document.querySelectorAll('.faq-lista dt');
+  }
+
+  faqMake() {
+    this.faqList.forEach((faq) => {
+      const dd = faq.nextElementSibling;
+      faq.addEventListener('click', () => {
+        dd.classList.toggle('showDD');
+      });
     });
-  });
+  }
+
+  initAcordiao() {
+    console.log(this.faqList);
+  }
 }
