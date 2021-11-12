ccf362d38a2e99411011f7d1a535a4c0fa65204b
Nicolas peluso <pelusonicolas7@gmail.com>
2021-11-12 05:37:04 -0300

    refatoração Horario de Funcionameento

--- js/modules/Funcionamento.js
+++ js/modules/Funcionamento.js
@@ -1,17 +1,25 @@
-export default function initFuncionamento() {
-  const EstaAberto = document.querySelector('.dados');
-  const funcionamento = document.querySelector('[data-Semana]');
-  const diasSemana = funcionamento.dataset.semana.split(',').map((item) => +item);
-  const HorarioSemana = funcionamento.dataset.horario.split(',').map((item) => +item);
+export default class Functionamentos {
+  constructor(dados, semana) {
+    this.EstaAberto = document.querySelector(dados);
+    this.funcionamento = document.querySelector(semana);
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map((item) => +item);
+    this.HorarioSemana = this.funcionamento.dataset.horario.split(',').map((item) => +item);
 
-  const DataAgora = new Date();
-  const DiaAgora = DataAgora.getDay();
-  const HorarioAgora = DataAgora.getHours();
+    this.DataAgora = new Date();
+    this.DiaAgora = this.DataAgora.getDay();
+    this.HorarioAgora = this.DataAgora.getUTCHours() - 3;
+  }
 
-  if (diasSemana.includes(DiaAgora) && HorarioAgora >= HorarioSemana[0]
-    && HorarioAgora < HorarioSemana[1]) {
-    EstaAberto.classList.add('aberto');
-  } else {
-    EstaAberto.classList.add('fechado');
+  wasWorking() {
+    if (this.diasSemana.includes(this.DiaAgora) && this.HorarioAgora >= this.HorarioSemana[0]
+      && this.HorarioAgora < this.HorarioSemana[1]) {
+      this.EstaAberto.classList.add('aberto');
+    } else {
+      this.EstaAberto.classList.add('fechado');
+    }
+  }
+
+  init() {
+    this.wasWorking();
   }
 }
